9767d24a6427ad041428cce7c4e4e3d48aa5c0d9
Amath <amathndaoseck@gmail.com>
2024-05-02 14:41:31 +0200

    � NEW: code postman

---
+++ file.py
@@ -0,0 +1,53 @@
+response_data = {
+    "knowncount": [
+        {
+            "id": "planet",
+            "knownCount": 8,
+            "updateDate": "24/08/2006",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/planet"
+        },
+        {
+            "id": "dwarfPlanet",
+            "knownCount": 5,
+            "updateDate": "24/08/2006",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/dwarfPlanet"
+        },
+        {
+            "id": "asteroid",
+            "knownCount": 1113527,
+            "updateDate": "21/11/2021",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/asteroid"
+        },
+        {
+            "id": "comet",
+            "knownCount": 3743,
+            "updateDate": "08/08/2021",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/comet"
+        },
+        {
+            "id": "moonsPlanet",
+            "knownCount": 285,
+            "updateDate": "01/08/2023",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/moonsPlanet"
+        },
+        {
+            "id": "moonsDwarfPlanet",
+            "knownCount": 9,
+            "updateDate": "02/05/2016",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/moonsDwarfPlanet"
+        },
+        {
+            "id": "moonsAsteroid",
+            "knownCount": 558,
+            "updateDate": "21/11/2021",
+            "rel": "https://api.le-systeme-solaire.net/rest/knowncount/moonsAsteroid"
+        }
+    ]
+}
+# Parcourir les éléments de knowncount
+for item in response_data["knowncount"]:
+    print("ID:", item["id"])
+    print("Known Count:", item["knownCount"])
+    print("Update Date:", item["updateDate"])
+    print("Rel:", item["rel"])
+    print("------------------------------------")
